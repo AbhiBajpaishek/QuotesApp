d5a763feac55d93c9bf6f0254c975803673e5acb
AbhiBajpaishek <abhishekbajpai449@gmailcom>
2021-11-28 19:49:29 +0530

    Routing Issue resolved

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React,{useEffect, useCallback} from 'react';
-import { Routes, Route, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
-import Quotes from "./components/Quotes/quotes";
 import Header from "./components/UI/header";
-import QuotesDetail from "./components/QuotesDetail/QuotesDetail";
-import AddQuote from "./components/AddQuote/AddQuote";
-import Signup from './components/Signup/Signup';
 import useFirebase from "./hooks/use-firebase";
 import {quotesActions} from './store/quotesSlice';
 import { commentsActions } from './store/commentsSlice';
 import { userActions } from './store/userSlice';
 import "./App.css";
-import Login from './components/Login/Login';
+import AppRoute from './components/Helper/AppRoute';
 
 function App() {
 
@@ -52,44 +47,7 @@ function App() {
     <div className="App">
       <Header></Header>
         <main>
-          <Routes>
-            { isLoggedIn &&
-              <Route path="/quotes" element={<Quotes></Quotes>} />
-            }
-            { isLoggedIn &&
-              <Route path="/addQuotes" element={<AddQuote />} />
-            }
-            { isLoggedIn && 
-            <Route
-              path="quotes/:quoteID/*"
-              element={<QuotesDetail></QuotesDetail>}
-              />
-            }
-            { !isLoggedIn && 
-              <Route path="/" element={<Navigate to="/login"></Navigate>} />
-              &&
-              <Route path = "/quotes" element = {<Navigate to = "/login"></Navigate>}/>
-              &&
-              <Route path = "/signup" element = {<Navigate to = "/signup"></Navigate>}/>
-              &&
-              <Route path = "*" element = {<Navigate to = "/login"></Navigate>}/>
-              &&
-              <Route path="/signup" element={<Signup />} />
-            }
-            {
-              isLoggedIn &&
-              <Route path="/" element={<Navigate to="/quotes"></Navigate>} />
-              &&
-              <Route path="/login" element={<Navigate to="/quotes"></Navigate>} />
-            }
-            { !isLoggedIn &&
-              <Route path="/login" element={<Login />} />
-            }
-            { isLoggedIn && 
-              <Route path="/signup" element={<Signup />} />
-            }
-            <Route path="*" element={<p>Page which you are looking for doesn't exist!!!!</p>} />
-          </Routes>
+          <AppRoute status = {isLoggedIn}></AppRoute>
         </main>
     </div>
   );
---
+++ src/components/Helper/AppRoute.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Quotes from "../Quotes/quotes";
+import QuotesDetail from "../QuotesDetail/QuotesDetail";
+import AddQuote from "../AddQuote/AddQuote";
+import Signup from "../Signup/Signup";
+import Login from "../Login/Login";
+
+const AppRoute = (props) => {
+  const isLoggedIn = props.status;
+  let jsx;
+  if (isLoggedIn) {
+    jsx = (
+      <Routes>
+        <Route path="/" element={<Navigate to="/quotes"></Navigate>} />
+        <Route path="*" element={<Navigate to="/quotes"></Navigate>} />
+        <Route path="/login" element={<Navigate to="/quotes"></Navigate>} />
+        <Route path="/signup" element={<Navigate to="/quotes"></Navigate>} />
+        <Route
+          path="quotes/:quoteID/*"
+          element={<QuotesDetail></QuotesDetail>}
+        />
+        <Route path="/quotes" element={<Quotes></Quotes>} />
+        <Route path="/addQuotes" element={<AddQuote />} />
+      </Routes>
+    );
+  } else {
+    jsx = (
+      <Routes>
+        <Route path="/" element={<Navigate to="/login"></Navigate>} />
+        <Route path="/quotes" element={<Navigate to="/login"></Navigate>} />
+        <Route path="*" element={<Navigate to="/login"></Navigate>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    );
+  }
+  return jsx;
+};
+
+export default AppRoute;
